39ea5aab486a97c75a87f2c01dca0c1752452693
Mofeed Nagib <mofeed.nagib@yale.edu>
2023-11-29 22:35:00 -0500

    more updates

---
+++ 90_run_analysis.R
@@ -0,0 +1,15 @@
+#===================#
+#=== RUN SCRIPTS ===#
+#===================#
+
+# Load Packages
+source("00_load_packages.R")
+
+# Get Data
+source("05_get_data.R")
+
+# Explore Data
+source("10_explore_data.R")
+
+# Fit Model
+# source("15_fit_model.R")
